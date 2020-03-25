29961ab6144fdd821767f7f35ad02c5aa595b5f9
JoaoVPereira <joaovicpmelo@gmail.com>
2020-03-24 22:17:48 -0300

    Imprimir os 100 primeiros numeros pares

---
+++ ImprimirCemPares.java
@@ -0,0 +1,10 @@
+package ExerciciosAula4;
+public class ImprimirCemPares {
+    public static void main(String...args){
+        
+        for (int i = 0; i <=100; i = i+2){
+            System.out.println(i);
+        }
+    }
+}
+
