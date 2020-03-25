ec62a25305b976f2ec33d7192fe5c05ac811df47
JoaoVPereira <joaovicpmelo@gmail.com>
2020-03-24 22:17:22 -0300

    Contagem Regressiva de 100 a 0

---
+++ ImprimirCemAUm.java
@@ -0,0 +1,11 @@
+package ExerciciosAula4;
+public class ImprimirCemAUm {
+    public static void main(String...args){
+        
+        for(int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
+        
+
+    }
+}
